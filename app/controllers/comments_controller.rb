f80a66c1456e048683c538d7e296c30bd54b2944
Kim Daekwon <propellerheaven@gmail.com>
2013-08-29 16:05:25 +0900

    Body 여백 적용 #4

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!, except: [:index, :show]
 
+
   # GET /comments
   # GET /comments.json
   def index
